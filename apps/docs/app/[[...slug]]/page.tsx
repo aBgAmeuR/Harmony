c7e26a64ae4a660514558f6700fd4d00d83e544f
aBgAmeuR <a.josset@yahoo.com>
2025-07-29 16:42:46 +0200

    feat(docs): enhance documentation structure and navigation (#105)

--- apps/docs/app/[[...slug]]/page.tsx
+++ apps/docs/app/[[...slug]]/page.tsx
@@ -20,7 +20,11 @@ export default async function Page(props: {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      tableOfContent={{ style: 'clerk' }}
+      full={page.data.full}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
